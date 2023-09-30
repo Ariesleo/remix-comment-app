21e298baea7c2f05b8ed4c8e20e2a97de8be7f0e
Murari Gupta <alexgupta2367@gmail.com>
2023-10-01 03:20:02 +0545

    added topnav

--- app/root.tsx
+++ app/root.tsx
@@ -14,6 +14,7 @@ import {
 
 import styles from '~/styles/app.css';
 import { Button } from './components/atoms/Button';
+import TopNav from './components/TopNav';
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
@@ -30,7 +31,12 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        <header>
+          <TopNav />
+        </header>
+        <main className="center-content">
+          <Outlet />
+        </main>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
