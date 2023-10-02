2d9967f9008ec2e2c4bb21afd03bc4acc8642000
Murari Gupta <alexgupta2367@gmail.com>
2023-10-02 10:25:17 +0545

    displaying proper error messagefor empty comments response

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,7 +3,8 @@ import Comment from '~/components/Comment';
 import { fetchComments } from '~/services/comment';
 import { LoaderFunctionArgs, json } from '@remix-run/node';
 import { getUserFromSession, requireUserSession } from '~/utils/session';
-import { useLoaderData } from '@remix-run/react';
+import { useLoaderData, useNavigate } from '@remix-run/react';
+import { Button } from '~/components/atoms/Button';
 
 export async function loader({ request }: LoaderFunctionArgs) {
   await requireUserSession(request);
@@ -14,14 +15,7 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
   try {
     const responseData = await fetchComments(token);
-    if (responseData?.data?.data.length <= 0) {
-      throw json(
-        { message: 'No data available' },
-        {
-          statusText: 'No data available to display',
-        }
-      );
-    }
+
     return responseData.data.data;
   } catch (err: any) {
     const errorMessage = err.response?.data?.message || 'An error occurred';
@@ -36,29 +30,49 @@ export async function loader({ request }: LoaderFunctionArgs) {
 const ViewComments = () => {
   const data = useLoaderData<typeof loader>() as any;
   const totalComments = data.length;
+  const isEmpty = data.length <= 0;
+
+  const navigate = useNavigate();
 
   return (
-    <div style={{ height: '90vh', overflowY: 'auto' }} className="scrollbar">
-      <section className=" dark:bg-gray-900 py-8 lg:py-16 antialiased">
-        <div className="max-w-2xl mx-auto px-4">
-          <div className="flex justify-between items-center mb-6">
-            <h2 className="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">
-              Discussion ({totalComments})
-            </h2>
-          </div>
-          {data.map((element: any) => {
-            return (
-              <Comment
-                key={element.id}
-                name={element.commenterName}
-                content={element.content}
-                date={element.createdAt}
-              />
-            );
-          })}
+    <>
+      {isEmpty ? (
+        <div>
+          <h2>Oh! snap. No data available</h2>
+          <Button
+            label="add comment"
+            onClick={() => {
+              navigate('/addcomments');
+            }}
+          />
         </div>
-      </section>
-    </div>
+      ) : (
+        <div
+          style={{ height: '90vh', overflowY: 'auto' }}
+          className="scrollbar"
+        >
+          <section className=" dark:bg-gray-900 py-8 lg:py-16 antialiased">
+            <div className="max-w-2xl mx-auto px-4">
+              <div className="flex justify-between items-center mb-6">
+                <h2 className="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">
+                  Discussion ({totalComments})
+                </h2>
+              </div>
+              {data.map((element: any) => {
+                return (
+                  <Comment
+                    key={element.id}
+                    name={element.commenterName}
+                    content={element.content}
+                    date={element.createdAt}
+                  />
+                );
+              })}
+            </div>
+          </section>
+        </div>
+      )}
+    </>
   );
 };
 
